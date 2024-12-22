0b1282815081de41dc6836d1968804b9e4e1c0b8
peeeeterd <148042500+peeeeterd@users.noreply.github.com>
2024-12-22 14:47:35 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lab10',
+  webDir: 'dist'
+};
+
+export default config;
